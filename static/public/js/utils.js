d4703cf69d73c186c3c5e623b839dcbfea694afe
Golodhrim <holidayninjastaff@gmail.com>
2022-04-22 03:19:42 -0400

    init design and styles commit for golang bookings project - vanilla js, html, and scss

---
+++ static/public/js/utils.js
@@ -0,0 +1,7 @@
+export function expandRooms() {
+	const el = document.getElementById('dropdown-content');
+	el.classList.toggle('show');
+}
+export function logHello() {
+	console.log('imported a hello');
+}
